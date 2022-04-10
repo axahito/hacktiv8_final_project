aa5a6a0428c804639f4cee8fdf859b388a4ff1de
Chris <abiyyuakahfi@gmail.com>
2022-04-10 20:46:44 +0700

    fix relationship migrations

--- models/comment.go
+++ models/comment.go
@@ -5,8 +5,10 @@ import "time"
 type Comment struct {
 	ID        int    `gorm:"primaryKey;autoIncrement"`
 	Message   string `gorm:"not null" json:"message" form:"message"`
-	UserID    int    `gorm:"not null" json:"user_id" form:"user_id"`
+	UserID    int    `gorm:"not null"`
+	User      User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	PhotoID   int    `gorm:"not null" json:"photo_id" form:"photo_id"`
+	Photo     Photo  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	UpdatedAt time.Time
 	CreatedAt time.Time
 }
--- models/social.go
+++ models/social.go
@@ -6,7 +6,8 @@ type Social struct {
 	ID        int    `gorm:"primaryKey;autoIncrement"`
 	Name      string `gorm:"not null" json:"name" form:"name"`
 	SocialURL string `gorm:"not null" json:"social_url" form:"social_url"`
-	UserID    int    `gorm:"not null" json:"user_id" form:"user_id"`
+	UserID    int    `gorm:"not null"`
+	User      User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	UpdatedAt time.Time
 	CreatedAt time.Time
 }
