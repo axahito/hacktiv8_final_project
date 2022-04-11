94458e724d0d48d7f8e10921d17d85a9e0b05e55
Chris <abiyyuakahfi@gmail.com>
2022-04-11 18:47:19 +0700

    create mapsocial fucntion

--- dto/social.go
+++ dto/social.go
@@ -1,8 +1,11 @@
 package dto
 
-import "time"
+import (
+	"final_project/models"
+	"time"
+)
 
-type Social struct {
+type social struct {
 	ID        int    `gorm:"primaryKey;autoIncrement"`
 	Name      string `gorm:"not null" json:"name" form:"name"`
 	SocialURL string `gorm:"not null" json:"social_url" form:"social_url"`
@@ -10,4 +13,25 @@ type Social struct {
 	User      user   `json:"user"`
 	UpdatedAt time.Time
 	CreatedAt time.Time
+}
+
+func MapSocial(source []models.Social) []social {
+	var result []social
+
+	for _, v := range source {
+		result = append(result, social{
+			ID:        v.ID,
+			Name:      v.Name,
+			SocialURL: v.SocialURL,
+			CreatedAt: v.CreatedAt,
+			UpdatedAt: v.UpdatedAt,
+			UserID:    v.UserID,
+			User: user{
+				Email:    v.User.Email,
+				Username: v.User.Username,
+			},
+		})
+	}
+
+	return result
 }
